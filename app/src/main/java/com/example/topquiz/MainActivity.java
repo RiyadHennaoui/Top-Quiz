9bb69f976ea91c787715209e9833cf15b62b7375
riyad.hennaoui <riyad.hennaoui@gmail.com>
2019-03-12 10:57:11 +0100

    initialize variables, disable play button for wait for the input user name and string created in Layout of activity main.
    listner added for name user input and enabled play button.
    
    Transition to a new activity for play game named game activity with data feedback.

---
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.topquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+    private Button mHistoryButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize variables with find View by id
+
+        mGreetingText = findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = findViewById(R.id.activity_main_name_input);
+        mPlayButton = findViewById(R.id.activity_main_play_btn);
+        mHistoryButton = findViewById(R.id.activity_main_history);
+
+        // disable the button to wait for the input of name input.
+
+        mPlayButton.setEnabled(false);
+
+        // add Listner for name input.
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Play Button Enable if name input has 1 character.
+                mPlayButton.setEnabled(s.toString().length() != 0);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        // add Listner when use the Button.
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Save the user name.
+                String firstName = mNameInput.getText().toString();
+
+                // go in new activity
+
+
+                Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivityForResult(gameActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
+
+
+            }
+        });
+
+    }
+}
