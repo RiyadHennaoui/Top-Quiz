ca2d024398f4982c0d7a36f99f916d84f3c584c8
riyad.hennaoui <riyad.hennaoui@gmail.com>
2019-03-12 18:20:05 +0100

    Layout for GameActivity created with TextView for display Question, and 4 button for display proposal answers.
    Method endGame Created.
    
    creat Question and Question Bank class
    
    add DisplayQuestion and Questionbank method
    
    add displayQuestion, dispatchTouchEvent and recreates a EndGame Method

--- app/src/main/java/com/example/topquiz/GameActivity.java
+++ app/src/main/java/com/example/topquiz/GameActivity.java
@@ -2,16 +2,19 @@ package com.example.topquiz;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
-public class GameActivity extends AppCompatActivity implements View.OnClickListener{
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mTextViewQuestion;
     private Button mButtonAnswer1;
@@ -28,8 +31,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     public static final String BUNDLE_STATE_QUESTION = "currentQuestion";
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,10 +42,10 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         mButtonAnswer3 = findViewById(R.id.game_activity_btn3);
         mButtonAnswer4 = findViewById(R.id.game_activity_btn4);
 
-        if (savedInstanceState != null){
+        if (savedInstanceState != null) {
             mScore = savedInstanceState.getInt(BUNDLE_STATE_SCORE);
             mNumberOfQuestions = savedInstanceState.getInt(BUNDLE_STATE_QUESTION);
-        }else {
+        } else {
 
             mScore = 0;
             mNumberOfQuestions = 4;
@@ -58,18 +59,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void displayQuestion(Question question) {
 
-        mTextViewQuestion.setText(question.getQuestion());
-        mButtonAnswer1.setText(question.getChoise().get(0));
-        mButtonAnswer1.setOnClickListener(this);
-        mButtonAnswer2.setText(question.getChoise().get(1));
-        mButtonAnswer2.setOnClickListener(this);
-        mButtonAnswer3.setText(question.getChoise().get(2));
-        mButtonAnswer3.setOnClickListener(this);
-        mButtonAnswer4.setText(question.getChoise().get(3));
-        mButtonAnswer4.setOnClickListener(this);
-    }
+    // Created Question bank !
 
     private QuestionBank createQuestionBank() {
 
@@ -126,24 +117,108 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 question5, question6, question7, question8));
     }
 
-    // Dialog box created when game is end with current score
 
-    private void endGame(){
+    // methode to listen and show question in button and TextView.
+    private void displayQuestion(Question question) {
+
+        mTextViewQuestion.setText(question.getQuestion());
+        mButtonAnswer1.setText(question.getChoise().get(0));
+        mButtonAnswer1.setOnClickListener(this);
+        mButtonAnswer2.setText(question.getChoise().get(1));
+        mButtonAnswer2.setOnClickListener(this);
+        mButtonAnswer3.setText(question.getChoise().get(2));
+        mButtonAnswer3.setOnClickListener(this);
+        mButtonAnswer4.setText(question.getChoise().get(3));
+        mButtonAnswer4.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return mEnableTouchEvents && super.dispatchTouchEvent(ev);
+    }
+
+
+    // save score and current question.
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(BUNDLE_STATE_SCORE, mScore);
+        outState.putInt(BUNDLE_STATE_QUESTION, mNumberOfQuestions);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int index;
+
+        switch (v.getId()) {
+
+            case R.id.game_activity_btn1: {
+                index = 0;
+                break;
+            }
+
+            case R.id.game_activity_btn2: {
+                index = 1;
+                break;
+            }
+
+            case R.id.game_activity_btn3: {
+                index = 2;
+                break;
+            }
+
+            case R.id.game_activity_btn4: {
+                index = 3;
+                break;
+            }
+
+            default:
+                index = -1;
+                break;
+        }
+        if (index == mCurrentQuestion.getAnswerIndex()) {
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+            mScore++;
+        } else {
+            Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
+        }
+        mEnableTouchEvents = false;
+
+
+        // Passe à la question suivante quand même
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mEnableTouchEvents = true;
+                if (--mNumberOfQuestions == 0) {
+                    endGame();
+                } else {
+                    mCurrentQuestion = mQuestionBank.getQuestion();
+                    displayQuestion(mCurrentQuestion);
+                }
+            }
+        }, 2000);
+
+
+    }
+
+    // Dialog box created when game is end with current score
+    private void endGame() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-            builder.setTitle("Well done !")
-                    .setMessage("Your score is : " + mScore)
-                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            Intent intent = new Intent();
-                            intent.putExtra(BUNDLE_EXTRA_SCORE, mScore);
-                            setResult(RESULT_OK, intent);
-                            finish();
-                        }
-                    })
-                    .create()
-                    .show();
+        builder.setTitle("Well done !")
+                .setMessage("Your score is : " + mScore)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent();
+                        intent.putExtra(BUNDLE_EXTRA_SCORE, mScore);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                })
+                .create()
+                .show();
     }
 }
